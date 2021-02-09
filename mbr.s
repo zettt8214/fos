357eceaa99dcb6b01bae55f598de002297ae7fcf
zettt8214 <xiaohao8214@gmail.com>
2021-02-08 21:35:15 -0800

    first commit

---
+++ mbr.s
@@ -0,0 +1,43 @@
+;
+ ;LOADER_BASE_ADDR equ 0xA000
+ ;LOADER_START_SECTOR equ 0x2
+;------------------------------------------------------------
+SECTION MBR vstart=0x7c00
+    mov ax,cs
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    mov fs,ax
+    mov sp,0x7c00
+    mov ax,0xb800
+    mov gs,ax
+
+    mov     ax, 0600h
+    mov     bx, 0700h
+    mov     cx, 0               ; 左上角: (0, 0)
+    mov     dx, 184fh           ; 右下角: (80,25),
+                       ; VGA文本模式中，一行只能容纳80个字符，共25行
+                       ; 下标从0开始，所以0x18=24，0x4f=79
+     int     10h                 ; int 10h
+
+      ; 输出背景色绿色，前景色红色，并且跳动的字符串"1 MBR"
+    mov byte [gs:0x00],'1'
+    mov byte [gs:0x01],0xA4   ; A表示绿色背景闪烁，4表示前景色为红色
+
+    mov byte [gs:0x02],' '
+    mov byte [gs:0x03],0xA4
+
+    mov byte [gs:0x04],'M'
+    mov byte [gs:0x05],0xA4
+
+    mov byte [gs:0x06],'B'
+    mov byte [gs:0x07],0xA4
+
+    mov byte [gs:0x08],'R'    
+    mov byte [gs:0x09],0xA4
+
+
+    jmp $                       ; 通过死循环使程序悬停在此
+
+    times 510-($-$$) db 0
+    db 0x55,0xaa
