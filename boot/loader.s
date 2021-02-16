84084ff648b4db2ed2a34e475375e3633f2ec578
zettt8214 <xiaohao8214@gmail.com>
2021-02-16 20:00:16 +0800

    update boot

--- boot/loader.s
+++ boot/loader.s
@@ -25,6 +25,8 @@ GDT_SIZE equ $ - GDT_BASE
 GDT_LIMIT equ GDT_SIZE - 1
 times 60 dq 0   ;60 reserved segement descriptors
 
+total_mem_bytes dd 0    ; total_mem_bytes denotes memory size
+
 SELECTOR_CODE equ (0x0001 << 3) + TI_GDT + RPL0
 SELECTOR_DATA equ (0x0002 << 3) + TI_GDT + RPL0
 SELECTOR_VEDIO equ (0x0003 << 3) + TI_GDT + RPL0
@@ -32,19 +34,103 @@ SELECTOR_VEDIO equ (0x0003 << 3) + TI_GDT + RPL0
 ;gdt pointer 
 gdt_ptr dw GDT_LIMIT
         dd GDT_BASE
-message db 'loading...'
+
+ards_buf times 244 db 0
+ards_nr dw 0
+
+
+message db 'get memory error!'
 
 loader_start:
+    
+
+;get memory size
+;---------------------- int 0x15 -------------------------- 
+    xor ebx, ebx
+    mov edx, 0x534d4150
+    mov di, ards_buf
+
+.e820_men_get_loop:
+    mov eax, 0x0000e820
+    mov ecx, 20
+    int 0x15
+    
+    jc .try_0xe801
+
+    add di, cx
+    inc word [ards_nr]
+    cmp ebx, 0
+    jnz .e820_men_get_loop
+
+    mov cx, [ards_nr]
+    mov ebx, ards_buf
+    xor edx, edx
+
+.find_max_mem_area:
+    mov eax, [ebx]
+    add eax, [ebx + 8]
+    add ebx, 20
+    cmp edx, eax
+    jge .next_ards
+    mov edx, eax
+
+.next_ards:
+    loop .find_max_mem_area
+    jmp .mem_get_ok
+
+
+.try_0xe801:
+    mov ax, 0xe801
+    int 0x15
+    jc .try_0x88
+
+    mov cx, 1024
+    mul cx
+    shl edx, 16
+    and eax, 0x0000ffff
+    or edx, eax
+    add edx, 0x100000
+    mov esi, edx
+
+    xor eax, eax
+    mov ax, bx
+    mov ecx, 0x10000
+    mul ecx
+    add esi, eax
+
+    mov edx, esi
+    jmp .mem_get_ok
+
+
+.try_0x88:
+    mov ah, 0x88
+    int 0x15
+    jc .error_hlt
+
+    mov cx, 1024
+    mul cx
+    shl edx, 16
+    and eax, 0x0000ffff
+    or edx, eax
+    add edx, 0x100000
+    jmp .mem_get_ok
+
+.error_hlt:
     mov sp, LOADER_BASE_ADDR
     mov bp, message
-    mov cx, 10
+    mov cx, 17
     mov ax, 0x1301
     mov bx, 0x001f
     mov dx, 0x1800
     int 0x10
+    jmp start_protection_mode
+
+.mem_get_ok:
+    mov [total_mem_bytes], edx
+
 
 ;---------------------- Start Protection Mode----------------
-
+start_protection_mode:
     ;open A20
     in al,0x92
     or al,00000010b
