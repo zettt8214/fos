03d65017281f38fb3be78f0f099817451478b230
zettt8214 <xiaohao8214@gmail.com>
2021-02-19 19:33:10 +0800

    add loading kernel

--- boot/loader.s
+++ boot/loader.s
@@ -38,11 +38,6 @@ gdt_ptr dw GDT_LIMIT
 ards_buf times 244 db 0
 ards_nr dw 0
 
-
-message db 'get memory error!'
-
-
-
 loader_start:
 ;get memory size
 ;---------------------- int 0x15 -------------------------- 
@@ -101,7 +96,6 @@ loader_start:
     mov edx, esi
     jmp .mem_get_ok
 
-
 .try_0x88:
     mov ah, 0x88
     int 0x15
@@ -115,21 +109,11 @@ loader_start:
     add edx, 0x100000
     jmp .mem_get_ok
 
-.error_hlt:
-    mov sp, LOADER_BASE_ADDR
-    mov bp, message
-    mov cx, 17
-    mov ax, 0x1301
-    mov bx, 0x001f
-    mov dx, 0x1800
-    int 0x10
-    jmp start_protection_mode
-
 .mem_get_ok:
     mov [total_mem_bytes], edx
 
 ;start protection mode
-start_protection_mode:
+.start_protection_mode:
     ;open A20
     in al,0x92
     or al,00000010b
@@ -145,7 +129,58 @@ start_protection_mode:
 
     jmp dword SELECTOR_CODE:p_mode_start
 
+.error_hlt:
+    hlt
+
 [bits 32]
+
+p_mode_start:
+    mov ax, SELECTOR_DATA
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, LOADER_STACK_TOP
+    mov ax, SELECTOR_VEDIO
+    mov gs, ax
+    mov byte [gs:160], 'P'
+
+;--------------------load kernel-----------------------------
+    mov eax, KERNEL_START_SECTOR
+    mov ebx, KERNEL_BIN_BASE_ADDR
+    mov ecx, 500
+
+    call rd_disk_32
+
+
+;-------------------start paging mechanism--------------------
+    call setup_page
+    sgdt [gdt_ptr]
+    mov ebx, [gdt_ptr + 2]
+
+    ;move VEDIO segment to high 1G virtual address space
+    or dword [ebx + 0x18 + 4], 0xc0000000
+
+    add dword [gdt_ptr + 2], 0xc0000000
+    add esp, 0xc0000000
+
+    mov eax, PAGE_DIR_TABLE_POS
+    mov cr3, eax 
+    
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+
+    lgdt [gdt_ptr]
+    mov byte [gs:160], 'V'
+
+    jmp SELECTOR_CODE:enter_kernel
+
+enter_kernel:
+    call kernel_init
+    mov esp, 0xc009f000
+    jmp KERNEL_ENTRY_POINT
+
+
 ;-------------------------------------------------------
 ;@brief : setup page directory table and page table
 ;-------------------------------------------------------
@@ -166,8 +201,10 @@ setup_page:
 
     mov dword [PAGE_DIR_TABLE_POS], eax ;first entry
 
-    mov dword [PAGE_DIR_TABLE_POS + 0xc00], eax
     ;high 1G space will be used for kernel (0xc0000000 ~ 0xffffffff) - pyhsical memory 0~0x3fffff
+    mov dword [PAGE_DIR_TABLE_POS + 0xc00], eax
+    
+    mov dword [PAGE_DIR_TABLE_POS + 4096], eax
 
 ;create Page table entry
     mov ecx, 256
@@ -192,37 +229,124 @@ setup_page:
     add eax, 0x1000
     loop .create_kernel_pde
     ret
-    
-p_mode_start:
-    mov ax, SELECTOR_DATA
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov esp, LOADER_STACK_TOP
-    mov ax, SELECTOR_VEDIO
-    mov gs, ax
-    mov byte [gs:160], 'P'
+;end setup_page
 
-;start paging mechanism
-    call setup_page
-    sgdt [gdt_ptr]
-    mov ebx, [gdt_ptr + 2]
+;---------------------------------------------------------
+;@brief: read n sectors from disk
+;@param: 
+;   eax: start number of sector
+;   bx: write address
+;   cx: sector count which will read
+;---------------------------------------------------------
+rd_disk_32:
 
-    ;move VEDIO segment to high 1G virtual address space
-    or dword [ebx + 0x18 + 4], 0xc0000000
+  mov esi,eax
+  mov edi,ecx
 
-    add dword [gdt_ptr + 2], 0xc0000000
-    add esp, 0xc0000000
+  ;set sector numbers
+  mov dx, 0x1f2
+  mov al, cl
+  out dx, al
 
-    mov eax, PAGE_DIR_TABLE_POS
-    mov cr3, eax 
-    
-    mov eax, cr0
-    or eax, 0x80000000
-    mov cr0, eax
+  mov eax, esi
+  ;write lba addr to 0x1f3 ~ 0x1f6
+  mov dx, 0x1f3
+  out dx, al
 
-    lgdt [gdt_ptr]
-    mov byte [gs:160], 'V'
-    jmp $
+  mov dx, 0x1f4
+  shr eax, 8
+  out dx, al
 
+  mov dx, 0x1f5
+  shr eax, 8
+  out dx, al
 
+  mov dx, 0x1f6
+  shr eax, 8
+  and al, 00001111b ;set 23~24 bits of lbs
+  or al, 11100000b  ;set lbs mode
+  out dx, al
+
+  ;set read command
+  mov dx, 0x1f7
+  mov al, 0x20  ;read
+  out dx, al
+
+  ;examine the status of disk
+.not_ready:
+  in al, dx
+  and al, 0x88 
+  cmp al, 0x08 
+  jnz .not_ready
+
+  ;read data from port 0x1f0
+  mov ax, di
+  mov dx, 256 
+  mul dx
+  mov cx, ax  ;total times, 2 bytes per time
+  mov dx, 0x1f0
+
+.read_loop:
+  in ax, dx
+  mov [ebx], ax
+  add ebx, 2
+  loop .read_loop
+
+  ret
+;end rd_disk
+
+;---------------------------------------------------------
+;@brief: copy kernel to target address
+;---------------------------------------------------------
+kernel_init:
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+    xor edx, edx
+
+    mov dx,[KERNEL_BIN_BASE_ADDR + 42]      ;e_phentsize
+    mov ebx, [KERNEL_BIN_BASE_ADDR + 28]    ;e_phoff
+    add ebx, KERNEL_BIN_BASE_ADDR           ;e_phoff + base address -> Elf32_Phdr
+    mov cx, [KERNEL_BIN_BASE_ADDR + 44]     ;e_phnum
+
+.each_segment:
+    cmp byte [ebx + 0], PT_NULL
+    je .PTNULL
+
+    push dword [ebx + 16]           ;p_filesz
+    mov eax, [ebx + 4]              ;p_offset
+    add eax, KERNEL_BIN_BASE_ADDR   
+    push eax
+    push dword [ebx + 8]            ;p_vaddr
+
+    call mem_cpy
+    add esp, 12
+
+.PTNULL:
+    add ebx, edx
+    loop .each_segment
+
+    ret
+;end kernel_init
+
+;---------------------------------------------------------
+;@brief: copy menmory from source to destination
+;@param: dst    destination address
+;        src    source address
+;        size   memory size
+;---------------------------------------------------------
+mem_cpy:
+    cld
+    push ebp
+    mov ebp, esp
+    push ecx            ;save ecx
+
+    mov edi, [ebp + 8]  ;dst
+    mov esi, [ebp + 12] ;src
+    mov ecx, [ebp + 16] ;size
+    rep movsb
+
+    pop ecx
+    pop ebp
+
+    ret
